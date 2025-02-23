ddacdd8d16995666aee1c18ddde47751bdcdeebf
pengpongpong <2695968851@qq.com>
2025-02-23 15:38:31 +0800

    [add] 增加装备抽奖UI

--- assets/src/resource/weaponry/enum.ts
+++ assets/src/resource/weaponry/enum.ts
@@ -1,13 +1,13 @@
 // 武器装备稀有度定义
 export enum WeaponryRarityType {
-    Well = "well",              // 良好
-    Excellent = "excellent",    // 精良
-    Epic = "epic",              // 史诗
-    Legend = "legend",          // 传说
+    Well = "良好",              // 良好
+    Excellent = "精良",    // 精良
+    Epic = "史诗",              // 史诗
+    Legend = "传说",          // 传说
 }
 
 // 武器装备种类定义
 export enum WeaponryKind {
-    Weapon = "weapon",  // 武器
+    Weapon = "武器",  // 武器
     // 护甲 护腿 配饰等
 }
