a9937007f155d4c3864f94f7a5ef08a14aa7ae51
hxc-github <1076707510@qq.com>
2025-03-07 15:10:10 +0800

    [UPDATE]调整战斗界面代码结构

--- assets/src/battle/characterController.ts
+++ assets/src/battle/characterController.ts
@@ -1,49 +1,230 @@
 // 角色控制，包括英雄和敌军
 
-import { _decorator, Component, Label, Node, Sprite } from 'cc';
+import { _decorator, Component, dragonBones, Label, Node, resources, Sprite, SpriteFrame, tween, Vec2, Vec3 } from 'cc';
+import { AnimationType, BattleCharacterAttribute, CharacterCampType, CharacterStateType } from './kind';
 const { ccclass, property } = _decorator;
 
 
 @ccclass('characterController')
 export class characterController extends Component {
-    // 血条
-    private gBloodBarSprite: Sprite = null;
+    // 英雄属性
+    private gAttribute: BattleCharacterAttribute = null;
+    // 英雄阵营
+    private gCamp: CharacterCampType = null;
+    // 英雄状态
+    private gState: CharacterStateType = null;
+    // 英雄最初的血条，用来进行血条比例显示
+    private gOriginHealth: number = 0;
+    // 角色初始位置，用于攻击后回原来位置
+    private gOriginPosition: Vec2 = null;
+
+    // 人物名称节点
+    @property(Node)
+    private gNameNode: Node = null;
+
+    // 血条节点
+    @property(Node)
+    private gBloodBarNode: Node = null;
 
     // 受到的伤害
-    private gHurtLabel: Label = null;
+    @property(Node)
+    private gHurtNode: Node = null;
+
+    // 动画所在节点
+    @property(Node)
+    private gBodyNode: Node = null;
+    // 动画
+    private gBodyArmatureDisplay: dragonBones.ArmatureDisplay = null;
 
     protected onLoad(): void {
-        this.gBloodBarSprite = this.node.getChildByName("BloodBackground").getChildByName("Blood").getComponent(Sprite);
-        this.gHurtLabel = this.node.getChildByName("Hurt").getComponent(Label);
-    }
-
-    start() {
-
+        this.gOriginPosition = new Vec2(this.node.position.x, this.node.position.y);
+        this.gBodyArmatureDisplay = this.gBodyNode.getComponent(dragonBones.ArmatureDisplay);
     }
 
     update(deltaTime: number) {
 
     }
 
-    // 设置受伤值
-    public setHurt(attack: number) {
-        this.gHurtLabel.string = attack.toString();
-        this.gHurtLabel.node.active = true;
-        let timeOutID = setTimeout(() => {
-            this.gHurtLabel.node.active = false;
-            clearTimeout(timeOutID);
-        }, 500);
+    // 设置英雄属性，一般用于初始化人物使用
+    public init(attribute: BattleCharacterAttribute, camp: CharacterCampType, state: CharacterStateType) {
+        // 初始化属性
+        this.gAttribute = {
+            id: attribute.id,
+            name: attribute.name,
+            role: attribute.role,
+            imageName: attribute.imageName,
+            isLong: attribute.isLong,
+            health: attribute.health,
+            attack: attribute.attack,
+            defense: attribute.defense,
+            attackSpeed: attribute.attackSpeed,
+            criticalStrikeRate: attribute.criticalStrikeRate,
+            criticalStrike: attribute.criticalStrike,
+            skillIDs: attribute.skillIDs,
+        }
+        this.gOriginHealth = attribute.health;
+        this.gState = state;
+        this.gCamp = camp;
+
+        // 初始化动画
+        let skePath = "dragon/" + this.gAttribute.imageName + "/" + this.gAttribute.imageName + "_ske";
+        resources.load(skePath, dragonBones.DragonBonesAsset, (err, skeAsset) => {
+            if (err) {
+                console.error("Failed to load ske.json:", err);
+                return;
+            }
+            // 2. 加载纹理数据：tes.json
+            let texPath = "dragon/" + this.gAttribute.imageName + "/" + this.gAttribute.imageName + "_tex";
+            resources.load(texPath, dragonBones.DragonBonesAtlasAsset, (err, atlasAsset) => {
+                if (err) {
+                    console.error("Failed to load tes.json:", err);
+                    return;
+                }
+                // 设置加载的资源到 ArmatureDisplay
+                this.gBodyArmatureDisplay.dragonAsset = skeAsset;
+                this.gBodyArmatureDisplay.dragonAtlasAsset = atlasAsset;
+
+                // 骨骼名称
+                this.gBodyArmatureDisplay.armatureName = "Armature";
+                // 播放动画
+                this.wait();
+            })
+        })
+        // 设置人物名称
+
+        this.gNameNode.getComponent(Label).string = this.gAttribute.name;
+
+        // 设置人物血条
+        let imagePath = "battle/" + "blood-green" + "/spriteFrame";
+        if (this.gCamp == CharacterCampType.Enemy) {
+            imagePath = "battle/" + "blood-red" + "/spriteFrame";
+        }
+        resources.load(imagePath, SpriteFrame, (err, spriteFrame) => {
+            if (err) {
+                console.warn(err);
+                return;
+            }
+            this.gBloodBarNode.getComponent(Sprite).spriteFrame = spriteFrame;
+        });
+        // 左右对称旋转敌人图画
+        if (this.gCamp == CharacterCampType.Enemy) {
+            let originScale = this.gBodyNode.scale;
+            this.gBodyNode.setScale(new Vec3(-originScale.x, originScale.y, originScale.z));
+
+        }
     }
 
-    // 设置血条比例
-    public setBlood(healthRate: number) {
-        if (healthRate <= 0) {
-            this.gBloodBarSprite.fillRange = 0
-        } else if (healthRate > 1) {
-            this.gBloodBarSprite.fillRange = 1;
-        } else {
-            this.gBloodBarSprite.fillRange = healthRate;
+    // 获取人物ID
+    public getID() {
+        return this.gAttribute.id;
+    }
+    // 获取防御值
+    public getDefense() {
+        return this.gAttribute.defense;
+    }
+
+    // 获取状态
+    public getState() {
+        return this.gState;
+    }
+
+    // 英雄等待
+    public wait() {
+        this.gBodyArmatureDisplay.playAnimation(AnimationType.Standby, 0);
+    }
+
+    // 英雄移动，远程英雄不移动
+    public move(targetPos: Vec2, callbackFunc: () => void) {
+        if (this.gAttribute.isLong) {
+            return;
         }
+        // callbackFunc.bind(this);
+        if (this.gCamp == CharacterCampType.Hero) {
+            tween(this.node).to(0.3, { position: new Vec3(targetPos.x - 40, targetPos.y, 0) }).call(callbackFunc).start();
+        } else {
+            tween(this.node).to(0.3, { position: new Vec3(targetPos.x + 40, targetPos.y, 0) }).call(callbackFunc).start();
+        }
+    }
+
+    // 英雄攻击
+    public attack(targetNode: Node) {
+
+        // 播放攻击动画
+        this.gBodyArmatureDisplay.playAnimation(AnimationType.Attack, 1);
+        // 计算血量
+        // 是否暴击
+        var isCritical = Math.random() < parseFloat((this.gAttribute.criticalStrikeRate / 100).toFixed(2));
+        // 最终攻击力 = 攻击力-防御力
+        var targetCharacterController = targetNode.getComponent(characterController);
+        var targetDefense = targetCharacterController.getDefense();
+        var attack = isCritical ? (this.gAttribute.attack * parseFloat((this.gAttribute.criticalStrike / 100).toFixed(2)) - targetDefense) : (this.gAttribute.attack - targetDefense);
+        if (attack <= 0) attack = 0;
+        this.gBodyArmatureDisplay.once(dragonBones.EventObject.COMPLETE, () => {
+            // 敌方扣血
+            targetCharacterController.attacked(attack);
+        }, this);
+
+    }
+
+    // 英雄回位
+    public moveBack(callbackFunc: () => void) {
+        // callbackFunc.bind(this);
+
+        tween(this.node).to(0.3, { position: new Vec3(this.gOriginPosition.x - 40, this.gOriginPosition.y, 0) }).call(callbackFunc).start();
+    }
+    // 被攻击，包括受伤显示，生命衰减，血条比例
+    // @param hurt：受到的伤害值
+    public attacked(hurt: number): CharacterStateType {
+        // 动画效果先做
+        this.gBodyArmatureDisplay.playAnimation(AnimationType.Attacked, 1);
+        // 生命值受损
+        this.gAttribute.health = this.gAttribute.health - hurt;
+        if (this.gAttribute.health <= 0) {
+            this.gAttribute.health = 0;
+        }
+        
+        // 受伤值显示
+        this.gHurtNode.getComponent(Label).string = hurt.toString();
+        this.gHurtNode.active = true;
+        let timeOutID = setTimeout(() => {
+            this.gHurtNode.getComponent(Label).string = "";
+            this.gHurtNode.active = false;
+            clearTimeout(timeOutID);
+        }, 500);
+        // 血条比例缩减
+        var healthRate = 0;
+        if (this.gOriginHealth > 0) {
+            healthRate = this.gAttribute.health / this.gOriginHealth;
+        }
+        if (healthRate <= 0) {
+            this.gBloodBarNode.getComponent(Sprite).fillRange = 0
+        } else if (healthRate > 1) {
+            this.gBloodBarNode.getComponent(Sprite).fillRange = 1;
+        } else {
+            this.gBloodBarNode.getComponent(Sprite).fillRange = healthRate;
+        }
+        // 角色死亡
+        if (this.gAttribute.health == 0) {
+            this.died();
+        } else {
+            this.gBodyArmatureDisplay.once(dragonBones.EventObject.COMPLETE, () => {
+                this.wait();
+            }, this);
+        }
+
+        return this.gState;
+    }
+
+    // 设置死亡情况，3秒后隐藏该节点
+    public died() {
+        this.gState = CharacterStateType.Die;
+        this.gBodyArmatureDisplay.playAnimation("died", 1);
+        this.gBodyArmatureDisplay.once(dragonBones.EventObject.COMPLETE, () => {
+            let timeOutID = setTimeout(() => {
+                this.node.active = false;
+                clearTimeout(timeOutID);
+            }, 3000);
+        }, this);
     }
 }
 
---
+++ assets/src/battle/kind.ts
@@ -0,0 +1,52 @@
+/* 结构体 */
+
+import { Node } from "cc";
+import { RoleType } from "../resource/character/attribute";
+
+// 战斗界面英雄信息
+export interface BattleCharacter {
+    id: string;                 // 英雄ID
+    attackSpeed: number;        // 攻击速度，用来列表排序
+    state: CharacterStateType;  // 英雄状态
+    camp: CharacterCampType;    // 判断敌友
+    node: Node;                 // 英雄节点
+}
+
+// 战斗界面英雄属性
+export interface BattleCharacterAttribute {
+    id: string;
+    name: string;             // 人物名称
+    role: RoleType;           // 职业
+    imageName: string;        // 图像名称
+    isLong: boolean;           // 是否是远程
+    health: number;            // 生命值
+    attack: number;            // 攻击力
+    defense: number;           // 防御力
+    attackSpeed: number;       // 攻击速度
+    criticalStrikeRate: number; // 暴击率
+    criticalStrike: number;    // 暴击伤害
+
+    skillIDs: string[];        // 技能ID列表
+}
+
+// 状态
+export enum CharacterStateType {
+    Wait,   // 等待
+    Move,   // 移动
+    Attack, // 攻击
+    Die     // 死亡
+}
+
+// 阵营
+export enum CharacterCampType {
+    Hero,   // 英雄
+    Enemy   // 敌人
+}
+
+// 动画效果
+export enum AnimationType {
+    Standby = "standby",    // 站立等待
+    Attack = "attack",      // 攻击
+    Attacked = "attacked",  // 被攻击
+    Died = "died",          // 死亡
+}
