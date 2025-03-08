4fc3918341e775594fc96485d41ae4e8da314dd7
pengpongpong <2695968851@qq.com>
2025-03-08 12:32:55 +0800

    [add] 增加数据库玩家英雄结构定义

---
+++ assets/src/equipment/enhancement.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('enhancement')
+export class enhancement extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+
+    }
+}
+
