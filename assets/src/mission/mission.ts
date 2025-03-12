cc399352f1451330aa39e24bb7184f829992f5f2
pengpongpong <2695968851@qq.com>
2025-03-12 11:03:30 +0800

    [add] 增加任务与成就界面

---
+++ assets/src/mission/mission.ts
@@ -0,0 +1,15 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('mission')
+export class mission extends Component {
+
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+
+    }
+}
+
